d72bb9ad06e27af86aed1f754b582d15daadaa86
Dmitriy Krasnov <itsnot@myemail.com>
2022-11-03 21:48:31 +0700

    uncommented

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 type serverJSON struct {
@@ -75,38 +78,33 @@ func readConfig() {
 
 func main() {
 	readConfig()
-	//// define origin server URL
-	//originServerURL, err := url.Parse("http://127.0.0.1:3031")
-	//if err != nil {
-	//	log.Fatal("invalid origin server URL")
-	//}
 
-	//loadBalancer := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-	//	fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
-	//
-	//	// set req Host, URL and Request URI to forward a request to the origin server
-	//	req.Host = originServerURL.Host
-	//	req.URL.Host = originServerURL.Host
-	//	req.URL.Scheme = originServerURL.Scheme
-	//	req.RequestURI = "" // https://go.dev/src/net/http/client.go:217
-	//
-	//	// save the response from the origin server
-	//	originServerResponse, err := http.DefaultClient.Do(req)
-	//	if err != nil {
-	//		rw.WriteHeader(http.StatusInternalServerError)
-	//		_, _ = fmt.Print(rw, err)
-	//		return
-	//	}
-	//
-	//	for key, values := range originServerResponse.Header {
-	//		for _, value := range values {
-	//			rw.Header().Add(key, value)
-	//		}
-	//	}
-	//	// return response to the client
-	//	rw.WriteHeader(http.StatusOK)
-	//	io.Copy(rw, originServerResponse.Body)
-	//})
+	loadBalancer := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
 
-	//log.Fatal(http.ListenAndServe(":8080", loadBalancer))
+		// set req Host, URL and Request URI to forward a request to the origin server
+		req.Host = originServerURL.Host
+		req.URL.Host = originServerURL.Host
+		req.URL.Scheme = originServerURL.Scheme
+		req.RequestURI = "" // https://go.dev/src/net/http/client.go:217
+
+		// save the response from the origin server
+		originServerResponse, err := http.DefaultClient.Do(req)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Print(rw, err)
+			return
+		}
+
+		for key, values := range originServerResponse.Header {
+			for _, value := range values {
+				rw.Header().Add(key, value)
+			}
+		}
+		// return response to the client
+		rw.WriteHeader(http.StatusOK)
+		io.Copy(rw, originServerResponse.Body)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", loadBalancer))
 }
