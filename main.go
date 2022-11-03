fb0292867e0a7a9a6c755635ed989f39bc59fc1c
Dmitriy Krasnov <itsnot@myemail.com>
2022-11-03 18:41:22 +0700

    simpel reverse proxy

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func main() {
+	// define origin server URL
+	originServerURL, err := url.Parse("http://127.0.0.1:3031")
+	if err != nil {
+		log.Fatal("invalid origin server URL")
+	}
+
+	reverseProxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+
+		// set req Host, URL and Request URI to forward a request to the origin server
+		req.Host = originServerURL.Host
+		req.URL.Host = originServerURL.Host
+		req.URL.Scheme = originServerURL.Scheme
+		req.RequestURI = "" // https://go.dev/src/net/http/client.go:217
+
+		// save the response from the origin server
+		originServerResponse, err := http.DefaultClient.Do(req)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Print(rw, err)
+			return
+		}
+
+		// return response to the client
+		rw.WriteHeader(http.StatusOK)
+		io.Copy(rw, originServerResponse.Body)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", reverseProxy))
+}
