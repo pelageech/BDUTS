a929abaf0dbb0119919ca02b76dd7fc1c9642836
Dmitriy Krasnov <itsnot@myemail.com>
2022-11-03 21:27:53 +0700

    added readConfig

--- main.go
+++ main.go
@@ -1,55 +1,113 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"log"
-	"net/http"
 	"net/url"
-	"time"
+	"os"
+	"strings"
 )
 
-type server struct {
-	url string
+type serverJSON struct {
+	URL string
 }
 
-type config struct {
-	port string
+type configJSON struct {
+	Port string
+}
+
+var servers []*url.URL
+
+var port string
+
+func readConfig() {
+	/*
+		read servers config
+	*/
+	serversFile, err := os.Open("resources/servers.json")
+	if err != nil {
+		log.Fatal("Failed to open servers config: ", err)
+	}
+	defer serversFile.Close()
+
+	serversFileByte, err := io.ReadAll(serversFile)
+	if err != nil {
+		log.Fatal("Failed to read servers config file: ", err)
+	}
+
+	var serversJSON []serverJSON
+	err = json.Unmarshal(serversFileByte, &serversJSON)
+	if err != nil {
+		log.Fatal("Failed to unmarshal servers config: ", err)
+	}
+
+	for _, server := range serversJSON {
+		serverURL, err := url.Parse(server.URL)
+		if err != nil {
+			log.Fatal("Failed to parse server URL", err)
+		}
+
+		servers = append(servers, serverURL)
+	}
+
+	/*
+		read load balancer config
+	*/
+	lbConfigFile, err := os.Open("resources/config.json")
+	if err != nil {
+		log.Fatal("Failed to open load balancer config file: ", err)
+	}
+	defer lbConfigFile.Close()
+
+	lbConfigFileByte, err := io.ReadAll(lbConfigFile)
+	if err != nil {
+		log.Fatal("Failed to read load balancer config file: ", err)
+	}
+
+	var lbConfig configJSON
+	err = json.Unmarshal(lbConfigFileByte, &lbConfig)
+	if err != nil {
+		log.Fatal("Failed to unmarshal load balancer config: ", err)
+	}
+
+	port = strings.Clone(lbConfig.Port)
 }
 
 func main() {
-	// define origin server URL
-	originServerURL, err := url.Parse("http://127.0.0.1:3031")
-	if err != nil {
-		log.Fatal("invalid origin server URL")
-	}
+	readConfig()
+	//// define origin server URL
+	//originServerURL, err := url.Parse("http://127.0.0.1:3031")
+	//if err != nil {
+	//	log.Fatal("invalid origin server URL")
+	//}
 
-	loadBalancer := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+	//loadBalancer := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+	//	fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+	//
+	//	// set req Host, URL and Request URI to forward a request to the origin server
+	//	req.Host = originServerURL.Host
+	//	req.URL.Host = originServerURL.Host
+	//	req.URL.Scheme = originServerURL.Scheme
+	//	req.RequestURI = "" // https://go.dev/src/net/http/client.go:217
+	//
+	//	// save the response from the origin server
+	//	originServerResponse, err := http.DefaultClient.Do(req)
+	//	if err != nil {
+	//		rw.WriteHeader(http.StatusInternalServerError)
+	//		_, _ = fmt.Print(rw, err)
+	//		return
+	//	}
+	//
+	//	for key, values := range originServerResponse.Header {
+	//		for _, value := range values {
+	//			rw.Header().Add(key, value)
+	//		}
+	//	}
+	//	// return response to the client
+	//	rw.WriteHeader(http.StatusOK)
+	//	io.Copy(rw, originServerResponse.Body)
+	//})
 
-		// set req Host, URL and Request URI to forward a request to the origin server
-		req.Host = originServerURL.Host
-		req.URL.Host = originServerURL.Host
-		req.URL.Scheme = originServerURL.Scheme
-		req.RequestURI = "" // https://go.dev/src/net/http/client.go:217
-
-		// save the response from the origin server
-		originServerResponse, err := http.DefaultClient.Do(req)
-		if err != nil {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, _ = fmt.Print(rw, err)
-			return
-		}
-
-		for key, values := range originServerResponse.Header {
-			for _, value := range values {
-				rw.Header().Add(key, value)
-			}
-		}
-		// return response to the client
-		rw.WriteHeader(http.StatusOK)
-		io.Copy(rw, originServerResponse.Body)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", loadBalancer))
+	//log.Fatal(http.ListenAndServe(":8080", loadBalancer))
 }
