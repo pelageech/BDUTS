82c6b5a116b983eac0d4e9b71add793185557fcf
Dmitriy Krasnov <itsnot@myemail.com>
2022-12-04 16:18:04 +0700

    added time tracker for serv2

--- serv2/main.go
+++ serv2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -11,12 +12,25 @@ const maxClients = 2
 var semaphore = make(chan struct{}, maxClients)
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	requestReceived := time.Now()
+
 	semaphore <- struct{}{}
 	defer func() { <-semaphore }()
 
 	select {
 	case <-time.After(5 * time.Second):
-		if _, err := w.Write([]byte("hello from 3032")); err != nil {
+		requestProcessed := time.Now()
+
+		if _, err := fmt.Fprintln(w, "hello from 3032"); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		if _, err := fmt.Fprintf(w, "Request received: %s\n", requestReceived.String()); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		if _, err := fmt.Fprintf(w, "Request processed: %s\n", requestProcessed.String()); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		if _, err := fmt.Fprintf(w, "Request processing time: %s\n", requestProcessed.Sub(requestReceived).String()); err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
 	case <-req.Context().Done():
