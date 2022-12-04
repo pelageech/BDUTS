6698e0d5cf23814467094bd0d467f8568b82c573
Dmitriy Krasnov <itsnot@myemail.com>
2022-12-04 16:21:06 +0700

    added time tracker for serv_https

--- serv_https/main.go
+++ serv_https/main.go
@@ -13,11 +13,27 @@ const maxClients = 2
 var semaphore = make(chan struct{}, maxClients)
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	requestReceived := time.Now()
+
 	semaphore <- struct{}{}
 	defer func() { <-semaphore }()
 
 	time.Sleep(time.Second * 5)
-	fmt.Fprintf(w, "hello from 33\n")
+	
+	requestProcessed := time.Now()
+
+	if _, err := fmt.Fprintln(w, "hello from 33"); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	if _, err := fmt.Fprintf(w, "Request received: %s\n", requestReceived.String()); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	if _, err := fmt.Fprintf(w, "Request processed: %s\n", requestProcessed.String()); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	if _, err := fmt.Fprintf(w, "Request processing time: %s\n", requestProcessed.Sub(requestReceived).String()); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func main() {
