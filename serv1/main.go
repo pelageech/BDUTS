cbe6a8f65b897e492bd5f6b89a967fec4a6dbd2e
pelageech <va.oleg2015@yandex.ru>
2022-12-10 11:17:11 +0700

    improved server, the process is killed on context cancellation

--- serv1/main.go
+++ serv1/main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/exec"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	var stdout bytes.Buffer
 	log.Println("conn")
-	e := exec.Command("Graphics-exe.exe", "Sin X" /*, "--width", "5000", "--height", "3000"*/)
-	if err := e.Run(); err != nil {
+
+	e := exec.Command("Graphics-exe.exe", "Sin X", "--width", "5000", "--height", "3000")
+	e.Stdout = &stdout
+
+	if err := e.Start(); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	go func() {
+		<-req.Context().Done()
+		_ = e.Process.Kill()
+	}()
+
+	if err := e.Wait(); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	str, err := os.ReadFile("./output.svg")
-	if err == nil {
-		if _, err := fmt.Fprintln(w, string(str)); err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
+	if _, err := fmt.Fprintln(w, stdout.String()); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
