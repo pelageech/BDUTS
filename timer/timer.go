111988ba497ffd3dadacb1704e5136dce573cd84
pelageech <va.oleg2015@yandex.ru>
2023-02-05 12:05:39 +0700

    timer.go В модуль перемещены методы для подсчёта времени на бэкендах и полного времени путешествия запроса

---
+++ timer/timer.go
@@ -0,0 +1,38 @@
+package timer
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptrace"
+	"time"
+)
+
+func MakeRequestTimeTracker(req *http.Request) (*http.Request, *time.Duration) {
+	var start time.Time
+	var /* finish,*/ finishBackend time.Duration
+
+	trace := &httptrace.ClientTrace{
+		WroteHeaders: func() {
+			start = time.Now()
+		},
+
+		GotFirstResponseByte: func() {
+			finishBackend = time.Since(start)
+			//	finish = time.Since(start)
+			//	fmt.Printf("[%s] Time from start to first bytes: full trip: %v, backend: %v\n", req.URL, finish, finishBackend)
+		},
+	}
+
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+
+	return req, &finishBackend
+}
+
+func SaveTimerDataGotFromCache(cacheTime time.Duration) {
+	log.Println("Full round trip time: ", cacheTime)
+}
+
+func SaveTimeDataBackend(backendTime time.Duration, fullTime time.Duration) {
+	log.Println("Backend time: ", backendTime)
+	log.Println("Full round trip time: ", fullTime)
+}
