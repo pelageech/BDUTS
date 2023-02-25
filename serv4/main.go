f7fcaa3d7b4dc97aed182b72963c2c20c46f4c1b
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-25 19:16:58 +0700

    sfsdfsf

--- serv4/main.go
+++ serv4/main.go
@@ -66,7 +66,7 @@ func drawFractal(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Starting sefdsfsdfrver...")
+	fmt.Println("Starting sefdsffsfsdsdfrver...")
 
 	http.HandleFunc("/fractal.gif", drawFractal)
 
