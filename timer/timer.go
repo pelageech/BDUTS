74b13192b0db140f10d5cd5bbb5f52a97870bf56
pelageech <va.oleg2015@yandex.ru>
2023-03-05 18:45:54 +0700

    backends were replaced to a new directory

--- timer/timer.go
+++ timer/timer.go
@@ -9,7 +9,7 @@ import (
 
 func MakeRequestTimeTracker(req *http.Request) (*http.Request, *time.Duration) {
 	var start time.Time
-	var /* finish,*/ finishBackend time.Duration
+	var finishBackend time.Duration
 
 	trace := &httptrace.ClientTrace{
 		WroteHeaders: func() {
@@ -18,8 +18,6 @@ func MakeRequestTimeTracker(req *http.Request) (*http.Request, *time.Duration) {
 
 		GotFirstResponseByte: func() {
 			finishBackend = time.Since(start)
-			//	finish = time.Since(start)
-			//	fmt.Printf("[%s] Time from start to first bytes: full trip: %v, backend: %v\n", req.URL, finish, finishBackend)
 		},
 	}
 
