1e257319e81ab7a074e9680f626f590fb5a688d4
pelageech <va.oleg2015@yandex.ru>
2023-03-26 19:53:21 +0700

    prepared server for max-age

--- backends/serv2/main.go
+++ backends/serv2/main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	var stdout bytes.Buffer
-	log.Println("conn")
-	e := exec.Command("./Graphics-exe.exe", "Sin X", "--width", "2000", "--height", "1500")
-	e.Stdout = &stdout
+	w.Header().Add("cache-control", "max-age=10")
 
-	if err := e.Start(); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
-	go func() {
-		<-req.Context().Done()
-		_ = e.Process.Kill()
-	}()
-
-	if err := e.Wait(); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
-
-	if _, err := fmt.Fprintln(w, stdout.String()); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	time.Sleep(5)
+	if _, err := fmt.Fprintln(w, time.Now()); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
 }
 func main() {
